aa4e497364c041082a639763555d550ad2e9db83
Tammy <tammycollinse@gmail.com>
2022-04-12 08:52:01 -0400

    commit tree 4a9345e2acdc456d7b90640bddd808582e15aa5f

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    "#{self.genre}"
   end
 
   def drake_made_this
+    self.update(name: "Drake")
+    self.find_or_create_by_name(:name => "Drake")
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
